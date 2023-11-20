10794bad061ff7c42d97f10318c8733885651f33
pondzaa114 <pharadonsiriforwork@gmail.com>
2023-11-20 19:13:35 +0700

    Adjust Page

--- lib/main.dart
+++ lib/main.dart
@@ -38,28 +38,39 @@ class _MyHomePageState extends State<MyHomePage> {
         backgroundColor: Theme.of(context).colorScheme.inversePrimary,
         title: Text(widget.title),
       ),
-      body: Center(
-        child: Card(
-          child: Container(
-            height: 350.0,
-            width: 350,
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                Center(
-                  child: Image.network(
-                    'https://img.freepik.com/free-photo/side-view-smiley-woman-holding-hen_23-2149456919.jpg?w=2000&t=st=1700474527~exp=1700475127~hmac=f8e772c940c9fdc0f4fb4154c068702b8a1f4f7212f99528feb1014fe5f92f64', // Replace with your image URL
-                    height: 300.0, // Set the desired height for the image
-                    width: 400.0, // Set the desired width for the image
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.start,
+        children: [SizedBox(height: 60,),
+          Card(
+            child: Container(
+              height: 350.0,
+              width: 350,
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: [
+                  Center(
+                    child: Image.network(
+                      'https://img.freepik.com/free-photo/side-view-smiley-woman-holding-hen_23-2149456919.jpg?w=2000&t=st=1700474527~exp=1700475127~hmac=f8e772c940c9fdc0f4fb4154c068702b8a1f4f7212f99528feb1014fe5f92f64', // Replace with your image URL
+                      height: 300.0, // Set the desired height for the image
+                      width: 400.0, // Set the desired width for the image
+                    ),
                   ),
-                ),
-                Center(
-                  child: Text("Title"),
-                ),
-              ],
+                 
+                ],
+              ),
             ),
           ),
-        ),
+          SizedBox(
+            height: 30,
+          ),
+          Center(
+            child: Text(
+              "Diary Title",
+              style: TextStyle(fontSize: 30),
+            ),
+          )
+        ],
       ),
     );
   }
