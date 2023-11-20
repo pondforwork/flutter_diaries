42f7523998d057b961e91094d35846267d34036c
pondforwork <pharadonsiriforwork@gmail.com>
2023-11-20 17:08:03 +0700

    Test Create Card and Create Model Diaries

--- lib/main.dart
+++ lib/main.dart
@@ -13,18 +13,16 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
-       
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: const MyHomePage(title: 'Flutter Diaries'),
     );
   }
 }
 
 class MyHomePage extends StatefulWidget {
   const MyHomePage({super.key, required this.title});
-
 
   final String title;
 
@@ -33,46 +31,31 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-    
-      _counter++;
-    });
-  }
-
   @override
   Widget build(BuildContext context) {
-    
     return Scaffold(
       appBar: AppBar(
-        
         backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-       
         title: Text(widget.title),
       ),
       body: Center(
-       
-        child: Column(
-         
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
+        child: Card(
+          child: Container(
+            height: 350.0,
+            width: 350,
+            child: Column(
+              children: [
+                Image.network(
+                  'https://img.freepik.com/free-photo/side-view-smiley-woman-holding-hen_23-2149456919.jpg?w=2000&t=st=1700474527~exp=1700475127~hmac=f8e772c940c9fdc0f4fb4154c068702b8a1f4f7212f99528feb1014fe5f92f64', // Replace with your image URL
+                  height: 300.0, // Set the desired height for the image
+                  width: 400.0, // Set the desired width for the image
+                ),
+                Text("Title"),
+              ],
             ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
-            ),
-          ],
+          ),
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), 
     );
   }
 }
---
+++ lib/models/diaries.dart
@@ -0,0 +1,6 @@
+class Diaries {
+  String title;
+  String story;
+
+  Diaries(this.title,this.story);
+}
