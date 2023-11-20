fc8197c79bb549473477b5f01c326bbf7b35ba62
pondforwork <pharadonsiriforwork@gmail.com>
2023-11-20 18:15:43 +0700

    Stuck on center

--- lib/main.dart
+++ lib/main.dart
@@ -44,13 +44,18 @@ class _MyHomePageState extends State<MyHomePage> {
             height: 350.0,
             width: 350,
             child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
               children: [
-                Image.network(
-                  'https://img.freepik.com/free-photo/side-view-smiley-woman-holding-hen_23-2149456919.jpg?w=2000&t=st=1700474527~exp=1700475127~hmac=f8e772c940c9fdc0f4fb4154c068702b8a1f4f7212f99528feb1014fe5f92f64', // Replace with your image URL
-                  height: 300.0, // Set the desired height for the image
-                  width: 400.0, // Set the desired width for the image
+                Center(
+                  child: Image.network(
+                    'https://img.freepik.com/free-photo/side-view-smiley-woman-holding-hen_23-2149456919.jpg?w=2000&t=st=1700474527~exp=1700475127~hmac=f8e772c940c9fdc0f4fb4154c068702b8a1f4f7212f99528feb1014fe5f92f64', // Replace with your image URL
+                    height: 300.0, // Set the desired height for the image
+                    width: 400.0, // Set the desired width for the image
+                  ),
                 ),
-                Text("Title"),
+                Center(
+                  child: Text("Title"),
+                ),
               ],
             ),
           ),
