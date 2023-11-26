c84ff8494090f5d47f24bfc1a91233a979e1f94e
pondforwork <pharadonsiriforwork@gmail.com>
2023-11-26 22:48:37 +0700

    message scrollable

--- lib/screens/reading.dart
+++ lib/screens/reading.dart
@@ -12,46 +12,49 @@ class FormScreen extends StatelessWidget {
       appBar: AppBar(
         title: Text(imageData['text']),
       ),
-      body: Container(
-        child: Center(
+      body: SingleChildScrollView(
+        child: Container(
+          child: Center(
             child: Column(
-          children: [
-            Card(
-              child: Container(
-                height: 420.0,
-                width: 320,
-                child: Column(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  crossAxisAlignment: CrossAxisAlignment.center,
-                  children: [
-                    // const SizedBox(
-                    //   height: 10,
-                    // ),
-                    Center(
-                      child: Image.asset(
-                        imageData['image'],
-                        width: 300,
-                        height: 400,
-                        fit: BoxFit.cover,
-                      ),
-                      // child: Image.asset('image01.png')
+              children: [
+                Card(
+                  child: Container(
+                    height: 420.0,
+                    width: 320,
+                    child: Column(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      crossAxisAlignment: CrossAxisAlignment.center,
+                      children: [
+                        Center(
+                          child: Image.asset(
+                            imageData['image']!,
+                            width: 300,
+                            height: 400,
+                            fit: BoxFit.cover,
+                          ),
+                        ),
+                      ],
                     ),
-                  ],
+                  ),
                 ),
-              ),
+                const SizedBox(
+                  height: 30,
+                ),
+                Container(
+                  child: Center(
+                    child: Text(
+                      imageData['text']!,
+                      style: const TextStyle(
+                        fontSize: 30,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                  ),
+                )
+              ],
             ),
-            const SizedBox(
-              height: 30,
-            ),
-            Container(
-              child: Center(child: Text(
-                imageData['text'],
-                style: const TextStyle(
-                    fontSize: 30, fontWeight: FontWeight.bold),
-              )),
-            )
-          ],
-        )),
+          ),
+        ),
       ),
       floatingActionButton: FloatingActionButton(
         onPressed: () {
